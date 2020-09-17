8b5fd42f7974183d9ec7418e47982328c499d82f
Marcin Aman <maman@virtuslab.com>
2020-09-17 19:40:42 +0200

    Add klaxon example

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,35 +3,35 @@ import org.jetbrains.dokka.DokkaConfigurationImpl
 import org.jetbrains.dokka.DokkaSourceSetID
 import org.jetbrains.dokka.DokkaSourceSetImpl
 import org.jetbrains.dokka.analysis.*
-import org.jetbrains.dokka.links.DRI
 import org.jetbrains.dokka.utilities.DokkaConsoleLogger
 import org.jetbrains.dokka.utilities.DokkaLogger
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
-import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.descriptors.PackageFragmentDescriptor
 import org.jetbrains.kotlin.descriptors.impl.DeclarationDescriptorVisitorEmptyBodies
 import org.jetbrains.kotlin.resolve.scopes.DescriptorKindFilter
 import java.io.File
+import java.util.concurrent.TimeUnit
 
-class Visitor : DeclarationDescriptorVisitorEmptyBodies<String, Unit>() {
-    override fun visitPackageFragmentDescriptor(descriptor: PackageFragmentDescriptor?, data: Unit?): String {
-        val classes = descriptor?.
+class Visitor : DeclarationDescriptorVisitorEmptyBodies<List<String>, Unit>() {
+    override fun visitPackageFragmentDescriptor(descriptor: PackageFragmentDescriptor?, data: Unit?): List<String> {
+        return descriptor?.
             getMemberScope()?.
             getContributedDescriptors(DescriptorKindFilter.CLASSIFIERS) { true }?.
-            filterIsInstance<ClassDescriptor>()?.map { visitClassDescriptor(it, data) }.orEmpty()
-        return classes.joinToString { it }
+            filterIsInstance<ClassDescriptor>()?.flatMap { visitClassDescriptor(it, data) }.orEmpty()
     }
 
-    override fun visitClassDescriptor(descriptor: ClassDescriptor?, data: Unit?): String {
-        return descriptor?.name?.asString() ?: ""
+    override fun visitClassDescriptor(descriptor: ClassDescriptor?, data: Unit?): List<String> {
+        return listOfNotNull(descriptor?.name?.asString())
     }
 }
 
 fun main(args: Array<String>) {
+    val workingDir = createTempDir()
+    "git clone https://github.com/cbeust/klaxon.git".runCommand(workingDir)
     val sourceset = DokkaSourceSetImpl(
-            displayName = "sample",
-            sourceSetID = DokkaSourceSetID("scopeId", "sourceSetName"),
-            sourceRoots = setOf(File("/Users/marcinaman/Documents/AnalyseKotlin/src/main/kotlin"))
+            displayName = "klaxon",
+            sourceSetID = DokkaSourceSetID("klaxonScopeID", "klaxonJVM"),
+            sourceRoots = setOf(File("${workingDir.absolutePath}/klaxon/klaxon/src/main/kotlin"))
     )
     val dokkaConfiguration: DokkaConfiguration = DokkaConfigurationImpl(
             sourceSets = listOf(sourceset)
@@ -45,13 +45,22 @@ fun main(args: Array<String>) {
             .mapNotNull { facade.resolveSession.getPackageFragment(it) }
             .toList()
     val visitor = Visitor()
-    val result = visitor.run {
-        packageFragments.mapNotNull { it as? PackageFragmentDescriptor }.map {
+    val result: List<String> = visitor.run {
+        packageFragments.mapNotNull { it as? PackageFragmentDescriptor }.flatMap {
             visitPackageFragmentDescriptor(
                     it,
                     Unit
             )
         }
     }
-    println("result: ${result}")
+    println("Top 10 longest class names: ${result.map { it to it.length}.sortedByDescending { it.second }.take(10)}")
+}
+
+fun String.runCommand(workingDir: File) {
+    ProcessBuilder(*split(" ").toTypedArray())
+        .directory(workingDir)
+        .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+        .redirectError(ProcessBuilder.Redirect.INHERIT)
+        .start()
+        .waitFor(60, TimeUnit.MINUTES)
 }
